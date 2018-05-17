dcd7f85a0f9f9383353060733af21b9217723c67
skoulouzis <S.Koulouzis@uva.nl>
2018-05-17 19:46:19 +0200

    in GisActivity.configure configBean.getOgcServiceUri().equals("") was wrong: This only works for string not URIs

--- apache-taverna-plugin-gis-activity-ui/src/main/java/org/apache/taverna/gis/ui/serviceprovider/GisServiceProvider.java
+++ apache-taverna-plugin-gis-activity-ui/src/main/java/org/apache/taverna/gis/ui/serviceprovider/GisServiceProvider.java
@@ -14,160 +14,167 @@ import org.apache.taverna.gis.client.*;
 import net.sf.taverna.t2.servicedescriptions.AbstractConfigurableServiceProvider;
 import net.sf.taverna.t2.servicedescriptions.ConfigurableServiceProvider;
 import net.sf.taverna.t2.servicedescriptions.CustomizedConfigurePanelProvider;
-import net.sf.taverna.t2.workflowmodel.processor.activity.config.ActivityInputPortDefinitionBean;
-import net.sf.taverna.t2.workflowmodel.processor.activity.config.ActivityOutputPortDefinitionBean;
 
 public class GisServiceProvider extends AbstractConfigurableServiceProvider<GisServiceProviderConfig>
-		implements ConfigurableServiceProvider<GisServiceProviderConfig>,
-		CustomizedConfigurePanelProvider<GisServiceProviderConfig> {
+        implements ConfigurableServiceProvider<GisServiceProviderConfig>,
+        CustomizedConfigurePanelProvider<GisServiceProviderConfig> {
 
-	public GisServiceProvider() {
-		super(new GisServiceProviderConfig("", new ArrayList<String>()));
-	}
+    public GisServiceProvider() {
+        super(new GisServiceProviderConfig("", new ArrayList<String>()));
+    }
 
-	private static final URI providerId = URI
-			.create("http://cs.man.ac.uk/2016/service-provider/apache-taverna2-plugin-gis");
-	
-	private Logger logger = Logger.getLogger(AddGisServiceDialog.class);
-	
-	
-	/**
-	 * Do the actual search for services. Return using the callBack parameter.
-	 */
-	public void findServiceDescriptionsAsync(FindServiceDescriptionsCallBack callBack) {
-		// Use callback.status() for long-running searches
+    private static final URI PROVIDER_ID = URI
+            .create("http://cs.man.ac.uk/2016/service-provider/apache-taverna2-plugin-gis");
 
-		URI serviceURI = serviceProviderConfig.getOgcServiceUri();
+    private final Logger LOGGER = Logger.getLogger(AddGisServiceDialog.class);
 
-		callBack.status("Resolving service: " + serviceURI);
-		
-		List<GisServiceDesc> results = new ArrayList<GisServiceDesc>();
+    /**
+     * Do the actual search for services. Return using the callBack parameter.
+     *
+     * @param callBack
+     */
+    @Override
+    public void findServiceDescriptionsAsync(FindServiceDescriptionsCallBack callBack) {
+        // Use callback.status() for long-running searches
 
-		IGisClient gisServiceClient = GisClientFactory.getInstance().getGisClient(getConfiguration().getOgcServiceUri().toASCIIString());
-		 
-		List<String> processIdentifiers = serviceProviderConfig.getProcessIdentifiers();
-		
-		try {
-			
-			for (String processID : processIdentifiers)
-			{
-				GisServiceDesc service = new GisServiceDesc();
+        URI serviceURI = serviceProviderConfig.getOgcServiceUri();
 
-				// Populate the service description bean
-				service.setOgcServiceUri(getConfiguration().getOgcServiceUri());
-				service.setProcessIdentifier(processID);
-		
-				// TODO: Optional: set description (Set a better description)
-				service.setDescription(processID);
-				
-				// Get input ports
-				List<IPortDataDescriptor> inputList = gisServiceClient.getTaverna2InputPorts(processID);
+        callBack.status("Resolving service: " + serviceURI);
 
-		        service.setInputPortDefinitions(inputList);
-				
-		        // Get output ports
-		        
-		        List<IPortDataDescriptor> outputList = gisServiceClient.getTaverna2OutputPorts(processID);
-		        
-		        service.setOutputPortDefinitions(outputList);
-		        
-		        results.add(service);
+        List<GisServiceDesc> results = new ArrayList<>();
 
-				// partialResults() can also be called several times from inside
-				// for-loop if the full search takes a long time
-				callBack.partialResults(results);
-				
-			}
-			
-		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(null,
-					"Could not read the service definition from "
-							+ serviceURI + ":\n" + ex,
-					"Could not add service service",
-					JOptionPane.ERROR_MESSAGE);
+        IGisClient gisServiceClient = GisClientFactory.getInstance().getGisClient(getConfiguration().getOgcServiceUri().toASCIIString());
 
-			logger.error(
-					"Failed to list GWS processes for service: "
-							+ serviceURI, ex);
-		}
+        List<String> processIdentifiers = serviceProviderConfig.getProcessIdentifiers();
+
+        try {
+
+            for (String processID : processIdentifiers) {
+                GisServiceDesc service = new GisServiceDesc();
+
+                // Populate the service description bean
+                service.setOgcServiceUri(getConfiguration().getOgcServiceUri());
+                service.setProcessIdentifier(processID);
+
+                // TODO: Optional: set description (Set a better description)
+                service.setDescription(processID);
+
+                // Get input ports
+                List<IPortDataDescriptor> inputList = gisServiceClient.getTaverna2InputPorts(processID);
+
+                service.setInputPortDefinitions(inputList);
+
+                // Get output ports
+                List<IPortDataDescriptor> outputList = gisServiceClient.getTaverna2OutputPorts(processID);
+
+                service.setOutputPortDefinitions(outputList);
+
+                results.add(service);
+
+                // partialResults() can also be called several times from inside
+                // for-loop if the full search takes a long time
+                callBack.partialResults(results);
+
+            }
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null,
+                    "Could not read the service definition from "
+                    + serviceURI + ":\n" + ex,
+                    "Could not add service service",
+                    JOptionPane.ERROR_MESSAGE);
+
+            LOGGER.error(
+                    "Failed to list GWS processes for service: "
+                    + serviceURI, ex);
+        }
+
+        // No more results will be coming
+        callBack.finished();
+    }
+
+    /**
+     * Icon for service provider
+     *
+     * @return
+     */
+    @Override
+    public Icon getIcon() {
+        return GisServiceIcon.getIcon();
+    }
+
+    /**
+     * Name of service provider, appears in right click for 'Remove service
+     * provider'
+     *
+     * @return
+     */
+    @Override
+    public String getName() {
+        return "Geospatial Web Services";
+    }
+
+    @Override
+    public String toString() {
+        return "Geospatial Web Services " + getConfiguration().getOgcServiceUri();
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String getId() {
+        return PROVIDER_ID.toASCIIString();
+    }
+
+    @Override
+    protected List<? extends Object> getIdentifyingData() {
+        List<String> result = new ArrayList<>();
+
+        List<String> processIdentifiers = getConfiguration().getProcessIdentifiers();
+
+        for (String processID : processIdentifiers) {
+            result.add(getConfiguration().getOgcServiceUri() + processID);
+
+        }
+
+        //return Arrays.asList(getConfiguration().getOgcServiceUri(), getConfiguration().getProcessIdentifier());
+        //return Arrays.asList(getConfiguration().getOgcServiceUri());
+        return result;
+
+    }
+
+    @Override
+    public List<GisServiceProviderConfig> getDefaultConfigurations() {
+
+        List<GisServiceProviderConfig> myDefaultConfigs = new ArrayList<>();
         
-		// No more results will be coming
-		callBack.finished();
-	}
-	
-	/**
-	 * Icon for service provider
-	 */
-	public Icon getIcon() {
-		return GisServiceIcon.getIcon();
-	}
+        myDefaultConfigs.add(new GisServiceProviderConfig("http://dataminer-prototypes.d4science.org/wps/WebProcessingService?",
+                Arrays.asList("dataminer-prototypes.d4science.org")));
 
-	/**
-	 * Name of service provider, appears in right click for 'Remove service
-	 * provider'
-	 */
-	public String getName() {
-		return "Geospatial Web Services";
-	}
+        return myDefaultConfigs;
 
-	@Override
-	public String toString() {
-		return "Geospatial Web Services " + getConfiguration().getOgcServiceUri();
-	}
+    }
 
-	public String getId() {
-		return providerId.toASCIIString();
-	}
+    @Override
+    public void createCustomizedConfigurePanel(
+            final net.sf.taverna.t2.servicedescriptions.CustomizedConfigurePanelProvider.CustomizedConfigureCallBack<GisServiceProviderConfig> callBack) {
 
-	@Override
-	protected List<? extends Object> getIdentifyingData() {
-		List<String> result = new ArrayList<String>();
-		
-		List<String> processIdentifiers = getConfiguration().getProcessIdentifiers();
-		
-		for (String processID : processIdentifiers)
-		{
-			result.add(getConfiguration().getOgcServiceUri() + processID);
-			
-		}
-		
-		//return Arrays.asList(getConfiguration().getOgcServiceUri(), getConfiguration().getProcessIdentifier());
-		//return Arrays.asList(getConfiguration().getOgcServiceUri());
-		return result;
-		
-	}
+        @SuppressWarnings("serial")
+        AddGisServiceDialog addGISServiceDialog = new AddGisServiceDialog(null) {
 
+            @Override
+            protected void addRegistry(String serviceURL, List<String> processIdentifiers) {
+                GisServiceProviderConfig providerConfig = new GisServiceProviderConfig(serviceURL, processIdentifiers);
+                callBack.newProviderConfiguration(providerConfig);
 
-	@Override
-	public List<GisServiceProviderConfig> getDefaultConfigurations(){
-		
-		List<GisServiceProviderConfig> myDefaultConfigs = new ArrayList<GisServiceProviderConfig>();
-		
-		myDefaultConfigs.add(new GisServiceProviderConfig("http://localhost:8080/geoserver/ows", 
-				Arrays.asList("gs:StringConcatWPS")));
-		
-		return myDefaultConfigs;
-		
-	}
+            }
 
-	@Override
-	public void createCustomizedConfigurePanel(
-			final net.sf.taverna.t2.servicedescriptions.CustomizedConfigurePanelProvider.CustomizedConfigureCallBack<GisServiceProviderConfig> callBack) {
+        };
 
-		@SuppressWarnings("serial")
-		AddGisServiceDialog addGISServiceDialog = new AddGisServiceDialog(null) {
+        addGISServiceDialog.setVisible(true);
 
-			@Override
-			protected void addRegistry(String serviceURL, List<String> processIdentifiers) {
-				GisServiceProviderConfig providerConfig = new GisServiceProviderConfig(serviceURL, processIdentifiers);					
-				callBack.newProviderConfiguration(providerConfig);
-				
-			}
-			
-		};
+    }
 
-		addGISServiceDialog.setVisible(true);
-		
-	}
-	
 }
